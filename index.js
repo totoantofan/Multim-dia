31935857d4c11d87facbb1e87169333c0bbaefd3
totoantofan <anthonyfanara11@gmail.com>
2025-12-21 16:34:03 +0100

    Improve client setup and reminder functionality
    Refactor client initialization and reminder message.

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Bot en ligne"));
 app.listen(3000);
+
 const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
 
 client.once("ready", () => {
   console.log(`Bot connecté en tant que ${client.user.tag}`);
-
-  const channelId = "ID_DU_SALON"; // Remplace par l'ID du salon Discord
-  const interval = 60 * 60 * 1000; // 1 heure
-
-  setInterval(() => {
-    const channel = client.channels.cache.get(channelId);
-    if (channel) {
-      channel.send("⏰ Rappel : n'oublie pas ta tâche !");
-    }
-  }, interval);
 });
+
+const channelId = "ID_DU_SALON"; // Remplace par l’ID du salon Discord
+const interval = 60 * 60 * 1000; // 1 heure
+
+setInterval(() => {
+  const channel = client.channels.cache.get(channelId);
+  if (channel) {
+    channel.send("🔔 Rappel : n’oublie pas ta tâche !");
+  }
+}, interval);
 
 client.login(process.env.TOKEN);
