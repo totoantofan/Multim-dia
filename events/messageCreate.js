d36b34faff71d745ecb9a4d7e9ce5c6dea3afa48
totoantofan <anthonyfanara11@gmail.com>
2025-12-22 11:34:14 +0100

    Remove messageCreate event handler

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,9 +1,1 @@
-module.exports = {
-  name: 'messageCreate',
-  async execute(message) {
-    if (message.author.bot) return;
-    if (global.maintenance === true) {
-      await message.delete().catch(() => {});
-    }
-  },
-};
+
