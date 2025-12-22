31da0b128d7c81e3c0c2e357df99e887b7b4e58c
totoantofan <anthonyfanara11@gmail.com>
2025-12-22 10:22:18 +0100

    Prevent message processing during maintenance mode

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,7 +2,6 @@ module.exports = {
   name: 'messageCreate',
   async execute(message) {
     if (message.author.bot) return;
-
     if (global.maintenance === true) {
       await message.delete().catch(() => {});
     }
