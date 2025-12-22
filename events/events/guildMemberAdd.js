abdbf18f075b2d7e6517672baca68e672fc07439
totoantofan <anthonyfanara11@gmail.com>
2025-12-22 11:36:12 +0100

    Delete anti-raid logic in guildMemberAdd.js
    Remove anti-raid functionality from guildMemberAdd event.

--- events/events/guildMemberAdd.js
+++ events/events/guildMemberAdd.js
@@ -1,18 +1,1 @@
-let joins = [];
 
-module.exports = {
-  name: 'guildMemberAdd',
-  async execute(member) {
-    if (!global.protect) return;
-
-    const now = Date.now();
-    joins.push(now);
-    joins = joins.filter(t => now - t < 10000);
-
-    if (joins.length >= 5) {
-      try {
-        await member.kick('Anti-raid activé');
-      } catch (e) {}
-    }
-  },
-};
